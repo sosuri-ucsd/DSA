288ef73ba206f80c7c2a32e0f1c26bf6addd4a44
sosuri-ucsd <sosuri@ucsd.edu>
2026-06-22 21:10:55 +0530

    basic maths problems

--- arrays/frequency.py
+++ arrays/frequency.py
@@ -1,11 +1,20 @@
 # Count frequency of each element in the array
 def frequency(arr):
     dct = {}
+
     for elem in arr:
         if elem not in dct:
             dct[elem] = 1
         else:
             dct[elem] += 1
+
+    high_elem = arr[0]
+    low_elem = arr[0]
+        
     for elem in dct:
-        print (elem, dct[elem])
-    
+            if dct[elem] > dct[high_elem]:
+                high_elem = elem
+            if dct[elem] < dct[low_elem]:
+                low_elem = elem
+        
+    return high_elem, low_elem
