0ec26ddfc6b98b25d62791beeeb715af455ed83b
sosuri-ucsd <sosuri@ucsd.edu>
2026-06-22 23:29:05 +0530

    basic maths problems

---
+++ sorting/insertion.py
@@ -0,0 +1,8 @@
+def insertion(lst):
+    for i in range(1, len(lst)):
+        j = i
+        while lst[j-1] > lst[j] and j > 0:
+            lst[j-1], lst[j] = lst[j], lst[j-1]
+            j -=1
+    return lst
+
