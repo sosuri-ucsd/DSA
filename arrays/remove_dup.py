4456336f907837a5f262b1a0f781f967441222d6
sosuri-ucsd <sosuri@ucsd.edu>
2026-06-23 23:34:54 +0530

    basic maths problems

---
+++ arrays/remove_dup.py
@@ -0,0 +1,10 @@
+def remove_duplicate(arr):
+    if not arr:
+        return []
+    
+    i = 0
+    for j in range(1, len(arr)):
+        if arr[j] != arr[i]:
+            i += 1
+            arr[i] = arr[j]
+    return i + 1
