f862fb9fb0d04cd1a2d945bad396a68a2297b708
sosuri-ucsd <sosuri@ucsd.edu>
2026-06-24 22:45:41 +0530

    basic maths problems

--- arrays/single_number.py
+++ arrays/single_number.py
@@ -7,3 +7,18 @@ def singleNumber(nums):
         for num in nums:
             result ^= num
         return result
+
+
+# Brute hashmap way
+def singleNumber(nums):
+    hm = {}
+    for element in nums:
+        if element not in hm:
+            hm[element] = 1
+        else:
+            hm[element] += 1
+        
+    for element in hm:
+        if hm[element] == 1:
+            return element
+
