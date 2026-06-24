29658780bbae39ec54f186a10dedc74e82040da8
sosuri-ucsd <sosuri@ucsd.edu>
2026-06-24 22:27:38 +0530

    basic maths problems

---
+++ arrays/count_1s.py
@@ -0,0 +1,11 @@
+# Given an array that contains only 1 and 0 return the count of maximum consecutive ones in the array.
+def count_consective_ones(nums):
+    count = 0
+    maxi = 0
+    for i in range(len(nums)):
+        if nums[i] == 1:
+            count += 1
+        else:
+            count = 0
+        maxi = max(maxi, count)
+    return maxi
