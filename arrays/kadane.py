d87f06674bfdc941cb310e58300818fcec0d47fa
sosuri-ucsd <sosuri@ucsd.edu>
2026-06-25 21:18:29 +0530

    arrays easy problems

---
+++ arrays/kadane.py
@@ -0,0 +1,10 @@
+def kadane(nums):
+        maxi = float('-inf')
+        sum = 0
+        for i in range(len(nums)):
+            sum += nums[i]
+            if sum > maxi:
+                maxi = sum
+            if sum < 0:
+                sum = 0
+        return maxi
