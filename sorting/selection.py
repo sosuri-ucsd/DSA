b86d95d570951c8faeaeb77c2bf65e07b9d237b2
sosuri-ucsd <sosuri@ucsd.edu>
2026-06-30 21:52:20 +0530

    sorting revision

--- sorting/selection.py
+++ sorting/selection.py
@@ -2,9 +2,8 @@
 def selection(lst):
     for i in range(len(lst)-1):
         minpos = i
-        for j in range(i, len(lst)):
+        for j in range(i + 1, len(lst)):
             if lst[j] < lst[minpos]:
                 minpos = j
-        
         lst[i], lst[minpos] = lst[minpos], lst[i]
     return lst
