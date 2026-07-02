12c5064940600a7ae9a5b0fe614e5203810a9c3c
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-02 19:38:46 +0530

    sorting revision

--- sorting/insertion.py
+++ sorting/insertion.py
@@ -1,9 +1,9 @@
 # Insertion Sort
 def insertion(lst):
     for i in range(1, len(lst)):
-        j = i
-        while lst[j-1] > lst[j] and j > 0:
-            lst[j-1], lst[j] = lst[j], lst[j-1]
-            j -=1
-    return lst
-
+        key = lst[i]
+        j = i - 1
+        while j >= 0 and lst[j] > key:
+            lst[j+1] = lst[j]
+            j-=1
+        lst[j+1] = key
