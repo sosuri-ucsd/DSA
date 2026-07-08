e839934e9abab80c40162c47aab3b085896bfdf3
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-08 20:29:12 +0530

    sorting revision

--- arrays/array_union.py
+++ arrays/array_union.py
@@ -3,32 +3,30 @@ def union(nums1, nums2):
     return sorted(set12)
 
 def union2(nums1, nums2):
+    n = len(nums1)
+    m = len(nums2)
     i, j = 0, 0
     result = []
-    
-    while i < len(nums1) and j < len(nums2):
-        if nums1[i] < nums2[j]:
-            if not result or result[-1] != nums1[i]:
+
+    while i < n and j < m:
+        if nums1[i] <= nums2[j]:
+            if result == [] or result[-1] != nums1[i]:
                 result.append(nums1[i])
             i += 1
-        elif nums2[j] < nums1[i]:
-            if not result or result[-1] != nums2[j]:
+        else:
+            if result == [] or result[-1] != nums2[j]:
                 result.append(nums2[j])
             j += 1
-        else:
-            if not result or result[-1] != nums1[i]:
+    
+    while i < n:
+        if result == [] or result[-1] != nums1[i]:
                 result.append(nums1[i])
-            i += 1
-            j += 1
-    
-    while i < len(nums1):
-        if result[-1] != nums1[i]:
-            result.append(nums1[i])
         i += 1
-    
-    while j < len(nums2):
-        if result[-1] != nums2[j]:
-            result.append(nums2[j])
+
+    while j < m:
+        if result == [] or result[-1] != nums2[j]:
+                result.append(nums2[j])
         j += 1
     
     return result
+
