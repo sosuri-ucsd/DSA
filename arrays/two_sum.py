333130a3f3c9d0fa3a9f5f23399d939c32c9d86c
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-08 21:22:11 +0530

    sorting revision

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -19,3 +19,11 @@ def two_sum(nums, target):
           else:
                hm[num] = i
      return [-1, -1]
+
+
+def two_sum(arr, target):
+     for i in range(0, len(arr) - 1):
+          for j in range(i + 1, len(arr)):
+               if arr[i] + arr[j] == target:
+                    return [i, j]
+     return [-1, -1]
