db0e3c93c469cf4b163cbfbb9fa69c543e030010
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-09 20:04:54 +0530

    sorting revision

--- arrays/rearrange_by_sign.py
+++ arrays/rearrange_by_sign.py
@@ -14,3 +14,17 @@ def rearrange(arr):
         nums.append(neg[i])
 
     return nums
+
+
+def rerrange2(arr):
+    result = [0] * len(arr)
+    pos = 0
+    neg = 1
+    for i in range(len(arr)):
+        if arr[i] >= 0:
+            result[pos] = arr[i]
+            pos += 2
+        else:
+            result[neg] = arr[i]
+            neg += 2
+    return result
