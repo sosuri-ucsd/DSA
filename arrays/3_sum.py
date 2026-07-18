205d3d48d1cceee083875da58d568715655b24c1
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-18 21:52:48 +0530

    sorting revision

--- arrays/3_sum.py
+++ arrays/3_sum.py
@@ -24,3 +24,31 @@ def three_sum(arr):
                 result.add(tuple(temp))
             my_set.add(arr[j])
     return [list(ans) for ans in result]
+
+# OPTIMAL
+def three_sum(arr):
+    ans = []
+    n = len(arr)
+    arr.sort()
+    for i in range(n):
+        if i != 0 and arr[i] == arr[i-1]:
+            continue
+        j = i + 1
+        k = n - 1
+        while j < k:
+            total_sum = arr[i] + arr[j] + arr[k]
+            if total_sum < 0:
+                j += 1
+            elif total_sum > 0:
+                k -= 1
+            else:
+                temp = [arr[i], arr[j], arr[k]]
+                ans.append(temp)
+                j += 1
+                k -= 1
+                while j < k and arr[j] == arr[j-1]:
+                    j += 1
+                while j < k and arr[k] == arr[k+1]:
+                    k -= 1
+    return ans
+    
