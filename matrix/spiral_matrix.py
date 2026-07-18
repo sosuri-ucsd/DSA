0041dcba0fd1d603c54578c72a8dbba884ef012d
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-18 20:30:46 +0530

    sorting revision

--- matrix/spiral_matrix.py
+++ matrix/spiral_matrix.py
@@ -10,4 +10,23 @@ def spiral_matrix(matrix):
     right = len(matrix[0]) - 1
 
     while top <= bottom and left <= right:
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                result.append(matrix[bottom][i])
+            bottom -= 1
         
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                result.append(matrix[i][left])
+            left += 1
+            
+    return result
+
