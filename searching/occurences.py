24da48461539e1b2c7048117dc079a7f1f86e8a1
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-20 22:10:58 +0530

    sorting revision

---
+++ searching/occurences.py
@@ -0,0 +1,31 @@
+def occurences(arr, target):
+    def lower_bound(arr, target):
+        low = 0
+        high = len(arr) - 1
+        lb = len(arr)
+        while low <= high:
+            mid = low + (high - low) // 2
+            if arr[mid] >= target:
+                lb = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return lb
+    
+    def upper_bound(arr, target):
+        low = 0
+        high = len(arr) - 1
+        ub = len(arr)
+        while low <= high:
+            mid = low + (high - low) // 2
+            if arr[mid] > target:
+                ub = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return ub
+    
+    lower = lower_bound(arr, target)
+    upper = upper_bound(arr, target)
+
+    return upper - lower
