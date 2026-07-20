4743f92b88c66f6ee7b4cfb1b84bc7b6ab1c0feb
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-20 19:45:26 +0530

    sorting revision

--- searching/upper_bound.py
+++ searching/upper_bound.py
@@ -2,9 +2,9 @@ def upper_bound(arr, target):
     low = 0 
     high = len(arr) - 1
     ub = len(arr)
-    while low > high:
+    while low <= high:
         mid = (low + high) // 2
-        if arr[mid] <= target:
+        if arr[mid] > target:
             ub = mid
             high = mid - 1
         else:
