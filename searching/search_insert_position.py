a1f41126c3a03157cb441ff2697f4b6c79db029e
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-20 20:15:21 +0530

    sorting revision

---
+++ searching/search_insert_position.py
@@ -0,0 +1,13 @@
+# OPTIMAL
+def search_insert_pos(arr, target):
+    low = 0 
+    high = len(arr) - 1
+    lb = len(arr)
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] >= target:
+            lb = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+    return lb
