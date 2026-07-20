2f37def84539e1ed2c514590075a941ff9fa1d4b
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-20 20:52:28 +0530

    sorting revision

--- searching/first_and_last_occurence.py
+++ searching/first_and_last_occurence.py
@@ -1,33 +1,39 @@
 def first_and_last_occurence(arr, target):
     lst = []
-    if not lst:
-        return [-1, -1]
     for i in range(len(arr)):
         if arr[i] == target:
             lst.append(i)
+    if not lst:
+        return [-1, -1]
     return [lst[0], lst[-1]]
 
-def first_and_last_occurence(arr, target):
-    low = 0 
-    high = len(arr) - 1
-    lb = len(arr)
-    ub = len(arr)
-    while low <= high:
-        mid = low + (high - low) // 2
-        if arr[mid] >= target:
-            lb = mid
-            high = mid - 1
-        else:
-            low = mid + 1
-    
-    low = 0
-    high = len(arr) - 1
-    while low <= high:
-        mid = low + (high - low) // 2
-        if arr[mid] > target:
-            ub = mid
-            high = mid - 1
-        else:
-            low = mid + 1
-        
-    return [lb, ub]
+def first_and_last_occurrence(arr, target):
+    def lower_bound():
+        lb = len(arr)
+        low, high = 0, len(arr)-1
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] >= target:
+                lb = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return lb
+
+    def upper_bound():
+        ub = len(arr)
+        low, high = 0, len(arr)-1
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] > target:
+                ub = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return ub
+
+    lb = lower_bound()
+    ub = upper_bound()
+    if lb == len(arr) or arr[lb] != target:
+        return [-1, -1]
+    return [lb, ub - 1]
