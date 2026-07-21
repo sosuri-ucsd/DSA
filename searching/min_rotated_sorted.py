477bcee1f8ae87d03bbf5623afd1d95125d78400
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-21 22:54:25 +0530

    sorting revision

---
+++ searching/min_rotated_sorted.py
@@ -0,0 +1,14 @@
+def min_rotated_sorted_array(arr):
+    min = float('inf')
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if arr[mid] <= arr[high]:
+            mini = min(mini, arr[mid])
+            high = mid - 1
+        else:
+            mini = min(mini, arr[low])
+            low = mid + 1
+    return mini.
+ 
+
