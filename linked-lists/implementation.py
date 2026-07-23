d0609fe99dffeb7a9a917844a35cc59680924e39
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-23 21:08:35 +0530

    sorting revision

--- linked-lists/implementation.py
+++ linked-lists/implementation.py
@@ -60,16 +60,6 @@ class SinglyLinkedList:
             else:
                 print("Node not found")
 
-
-
-
-
-
-
-
-
-
-
 node1 = Node(5)
 node2 = Node(10)
 node3 = Node(7)
@@ -83,6 +73,27 @@ node4.next = None
 print(node1.val)
 print(node1.next.val)
 print(node2) # Same as line above
+
+
+
+def delete(self, val):
+    if self.head is None:
+        print("Not Found")
+        return
+    if self.head.val == val:
+        self.head = self.head.next
+        return
+    
+    prev = None
+    curr = self.head
+    while curr is not None:
+        if curr.val == val:
+            prev.next = curr.next
+            return
+        prev = curr
+        curr = curr.next
+
+    print("Not Found")
 
 
 
