6479d5b262185890366c6294a7dfbbf1ed033dc3
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-24 22:23:52 +0530

    sorting revision

--- linked-lists/odd_even.py
+++ linked-lists/odd_even.py
@@ -1,4 +1,4 @@
-def linkedlist_cycle2(self, head: ListNode) -> ListNode:
+def linkedlist_cycle2(self, head):
     temp = head
     values = []
     if head == None or head.next is None:
@@ -18,3 +18,18 @@ def linkedlist_cycle2(self, head: ListNode) -> ListNode:
           temp = temp.next
     return head
 
+
+
+def linkedlist_cycle2(self, head):
+    if head == None or head.next is None:
+        return head
+    odd = head
+    even = head.next
+    even_head = even
+    while even is not None and even.next is not None:
+         odd.next = odd.next.next
+         odd = odd.next
+         even.next = even.next.next
+         even = even.next
+    odd.next = even_head
+    return head
