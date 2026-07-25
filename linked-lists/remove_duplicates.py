a06b850442f70a44da2216095a44e9b552028d04
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-26 00:07:50 +0530

    sorting revision

---
+++ linked-lists/remove_duplicates.py
@@ -0,0 +1,15 @@
+def remove_duplicates(self, head):
+    if head is None:
+        return head
+    curr = head
+    while curr and curr.next:
+        if curr.val == curr.next.val:
+            dup = curr.next
+            curr.next = dup.next
+            if dup.next:
+                dup.next.prev = curr
+        curr = curr.next
+    return head
+
+
+
