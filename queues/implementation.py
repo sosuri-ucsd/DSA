8c9bd0d0af323e576d6d6683c0597799721cd163
sosuri-ucsd <sosuri@ucsd.edu>
2026-07-29 20:47:12 +0530

    sorting revision

--- queues/implementation.py
+++ queues/implementation.py
@@ -1,3 +1,24 @@
 class Queue:
     def __init__(self):
-        
+        self.items = []
+    def is_empty(self):
+        return len(self.items) == 0
+    def enqueue(self, item):
+        self.items.append(item)
+    def dequeue(self):
+        if len(self.items) == 0:
+            return "Not enough items"
+        else:
+            return self.items.pop(0)
+    def front(self):
+        if len(self.items) == 0:
+            return "Not enough items"
+        else:
+            return self.items[0]
+    def back(self):
+        if len(self.items) == 0:
+            return "Not enough items"
+        else:
+            return self.items[-1]
+    def size(self):
+        return len(self.items)
