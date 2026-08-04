689171bdde3baa95d0dc9c707f66a1b4db52d24b
sosuri-ucsd <sosuri@ucsd.edu>
2026-08-04 21:36:59 +0530

    sorting revision

---
+++ stacks/check_valid_parenthesis.py
@@ -0,0 +1,18 @@
+def check_valid(s):
+    stack = []
+    for bracket in s:
+        if bracket in "([{":
+            stack.append(bracket)
+        else:
+            if len(stack) == 0:
+                return False
+            ch = stack.pop()
+            if (
+                (bracket == ')' and ch == '(')
+                or (bracket == ']' and ch == '[')
+                or (bracket == '}' and ch == '{')
+            ):
+                continue
+            else:
+                return False
+    return len(stack) == 0
