994d42cdad09b740c21e293149372a5bead48dc0
sosuri-ucsd <sosuri@ucsd.edu>
2026-08-04 20:25:03 +0530

    sorting revision

---
+++ stacks/queue_using_stack.py
@@ -0,0 +1,27 @@
+class StackQueue:
+    def __init__(self):
+        self.st1 = []
+        self.st2 = []
+
+    def push(self, x):
+        while self.st1:
+            self.st2.append(self.st1.pop())
+        self.st1.append(x)
+        while self.st2:
+            self.st1.append(self.st2.pop())
+
+    def pop(self):
+        if not self.st1:
+            print("Stack is empty")
+            return -1
+        top_element = self.st1.pop()
+        return top_element
+
+    def peek(self):
+        if not self.s1:
+            print("Stack is empty")
+            return -1
+        return self.st1[-1]
+
+    def is_empty(self):
+        return not self.s1
